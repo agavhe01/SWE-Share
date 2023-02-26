d9e2eb671eb3bd9e24a43162942e20579217c92a
Anesu Gavhera <agavhera@Anesus-MacBook-Pro.local>
2023-02-26 00:28:09 -0500

    Redux Setup and Alert Set/Remove

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,31 @@ import Landing from './components/layout/Landing';
 
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/alert';
 import './App.css';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './components/store';
+
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Landing />} />
-      </Routes>
-      <section className='container'>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
         <Routes>
-          <Route exact path='/register' element={<Register />} />
-          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/' element={<Landing />} />
         </Routes>
-      </section>
-    </Fragment>
-  </Router>
+        <section className='container'>
+          <Alert />
+          <Routes>
+            <Route exact path='/register' element={<Register />} />
+            <Route exact path='/login' element={<Login />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
