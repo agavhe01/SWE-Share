bf128f17767a2c9f875e86f8ec824c13dd085a61
Anesu Gavhera <agavhera@Anesus-MacBook-Pro.local>
2023-02-26 21:52:13 -0500

    Profile Reducer and getting Profiles in Dash

---
+++ client/src/components/actions/profiles.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  CLEAR_PROFILE,
+  ACCOUNT_DELETED,
+  GET_REPOS,
+  NO_REPOS,
+} from './types';
+
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+// Get current users profile
+export const getCurrentProfile = () => async (dispatch) => {
+  try {
+    //const res = await api.get('/profile/me');
+
+    const res = await axios.get('api/profile/me');
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,8 +2,12 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { getCurrentProfile } from '../actions/profiles';
 
-const Dashboard = () => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
   return (
     <section className='container'>
       <h1 className='large text-primary'>Dashboard</h1>
@@ -36,8 +40,16 @@ const Dashboard = () => {
   );
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  // deleteAccount: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
 
-export default connect(mapStateToProps, {})(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
