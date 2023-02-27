641ae4542f9950d14169a95904629a30f5bde2c0
Anesu Gavhera <agavhera@Anesus-MacBook-Pro.local>
2023-02-27 12:28:27 -0500

    Preparing to Deploy

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // simple server
 const express = require('express');
 
+// path module
+const path = require('path');
+
 // Require the mongo DB
 const connectDB = require('./config/db');
 
@@ -11,16 +14,28 @@ const app = express();
 connectDB();
 
 /// Init middlewre
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
-// single endpoint
-app.get('/', (req, res) => res.send('API Running'));
+// single endpoint disabled at deploy
+//app.get('/', (req, res) => res.send('API Running'));
 
 // define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+
+// check for pod
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 6000;
 
